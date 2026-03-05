9b4258d8c1bb43d9537de71690b11346719b43d3
GGMV-2201 <gibrang.martinez@cbtis265.edu.mx>
2026-03-05 14:57:38 -0600

    a

---
+++ rendimientoA.dart
@@ -0,0 +1,23 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main(List<String> args) {
+  print("Sistema de rendimiento academico");
+
+  print("Cuántos alumnos vas a ingresar?");
+
+  int alumnos = int.parse(stdin.readLineSync()!);
+  int totalexcelentes = 0;
+  int totalreprobados = 0;
+
+  for (int i = 0; i < alumnos; i++) {
+    print("${i + 1}");
+    print("nombre del alumno");
+    String? name = stdin.readLineSync();
+    print("Cuantas materias va a cursar?");
+    int materias = int.parse(stdin.readLineSync()!);
+
+    double suma 
+    Doubl
+  }
+}
